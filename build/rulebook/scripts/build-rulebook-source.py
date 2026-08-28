8b77ea8207d76e86a781cd49cb4b35846ed6319b
Dan Weaver <dweaver@boulderbioconsulting.com>
2026-08-28 05:38:25 -0600

    Wire explicit ICE publication image overrides into Step 4

--- build/rulebook/scripts/build-rulebook-source.py
+++ build/rulebook/scripts/build-rulebook-source.py
@@ -7,6 +7,9 @@ from rulebook_step4_class_publication_images import (
 )
 from rulebook_step4_class_relationships import configure_step4_class_relationships
 from rulebook_step4_domain_semantics import configure_step4_domain_semantics
+from rulebook_step4_ice_publication_image_overrides import (
+    configure_step4_ice_publication_image_overrides,
+)
 from rulebook_step4_ice_publication_images import (
     configure_step4_ice_publication_images,
 )
@@ -23,6 +26,7 @@ def _configure(namespace):
     configure_step4_domain_semantics(namespace)
     configure_step4_prose_boundaries(namespace)
     configure_step4_ice_semantics(namespace)
+    configure_step4_ice_publication_image_overrides(namespace)
     configure_step4_ice_publication_images(namespace)
 
 
